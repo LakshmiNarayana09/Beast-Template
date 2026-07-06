607259a3eba5b45215f592c0285a1f9cc905df6f
Lakshmi Narayana <gnarayana122@gmail.com>
2026-07-06 11:29:46 +0530

    update

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Element } from "react-scroll";
+
 import img1 from "../../assets/gallery/img1.png";
 import img2 from "../../assets/gallery/img2.png";
 import img3 from "../../assets/gallery/img3.png";
@@ -12,7 +13,7 @@ function Gallery() {
 
   return (
     <Element name="gallery">
-      <section className="relative mb-[50px] h-[60vh] w-full sm:h-[70vh] lg:h-screen">
+      <section className="relative mb-[50px] h-[40vh] w-full sm:h-[50vh] md:h-[60vh] lg:h-screen">
         <img
           src={activeImage}
           alt="Gallery"
@@ -21,26 +22,24 @@ function Gallery() {
 
         <div className="absolute inset-0 bg-black/20"></div>
 
-        <div className="absolute bottom-4 left-1/2 z-10 -translate-x-1/2 lg:bottom-8">
-          <div className="flex justify-center gap-2 rounded-lg bg-black/40 p-2 backdrop-blur-sm lg:gap-5">
-            {images.map((image, index) => (
-              <button
-                key={index}
-                onClick={() => setActiveImage(image)}
-                className={`h-[55px] w-[80px] overflow-hidden transition-all duration-300 sm:h-[70px] sm:w-[120px] lg:h-[106px] lg:w-[182px] ${
-                  activeImage === image
-                    ? "scale-105 ring-2 ring-white"
-                    : "opacity-70 hover:opacity-100"
-                }`}
-              >
-                <img
-                  src={image}
-                  alt={`Thumbnail ${index + 1}`}
-                  className="h-full w-full object-cover"
-                />
-              </button>
-            ))}
-          </div>
+        <div className="absolute bottom-4 left-1/2 z-10 flex w-[95%] -translate-x-1/2 gap-2 overflow-x-auto rounded-lg bg-black/40 p-2 backdrop-blur-sm sm:w-auto sm:gap-3 lg:bottom-8 lg:gap-5">
+          {images.map((image, index) => (
+            <button
+              key={index}
+              onClick={() => setActiveImage(image)}
+              className={`h-[70px] w-[120px] flex-shrink-0 overflow-hidden transition-all duration-300 sm:h-[90px] sm:w-[150px] lg:h-[106px] lg:w-[182px] ${
+                activeImage === image
+                  ? "scale-105 ring-2 ring-white"
+                  : "opacity-70 hover:opacity-100"
+              }`}
+            >
+              <img
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+                className="h-full w-full object-cover"
+              />
+            </button>
+          ))}
         </div>
       </section>
     </Element>
